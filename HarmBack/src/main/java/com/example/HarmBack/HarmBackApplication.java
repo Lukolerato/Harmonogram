967cd2a685e0828a2234042fefb61f350880f36c
Dominika <dominika32110@gmial.com>
2025-03-15 15:33:17 +0100

    init SpringBoot

---
+++ HarmBack/src/main/java/com/example/HarmBack/HarmBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.HarmBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmBackApplication.class, args);
+	}
+
+}
