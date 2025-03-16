2ac7cf1c3031bb2b7907daac26059332f9de32f6
Dominika <dominika32110@gmial.com>
2025-03-16 12:44:51 +0100

    endpoint baza

--- HarmFront/src/App.js
+++ HarmFront/src/App.js
@@ -1,19 +1,25 @@
 import { useEffect, useState } from "react";
-import api from "./api";
+import { getTasks } from "./api";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    api.get("/hello")
-      .then(response => setMessage(response.data))
-      .catch(error => console.error("Błąd API:", error));
+    getTasks()
+      .then((data) => setTasks(data))
+      .catch((error) => console.error("Błąd API:", error));
   }, []);
 
   return (
     <div>
-      <h1>React + Spring Boot</h1>
-      <p>{message}</p>
+      <h1>Napis z bazy:</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <strong>{task.title}</strong>: {task.description}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
