d20dae123ed3f814716449c5e62e725d6d9fb2fe
Dominika <dominika32110@gmial.com>
2025-03-16 11:58:09 +0100

    init Demo Spring

--- HarmDemo/src/main/java/com/ConfHandler/HarmDemo/HarmDemoApplication.java
+++ HarmDemo/src/main/java/com/ConfHandler/HarmDemo/HarmDemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.HarmBack;
+package com.ConfHandler.HarmDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HarmBackApplication {
+public class HarmDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HarmBackApplication.class, args);
+		SpringApplication.run(HarmDemoApplication.class, args);
 	}
 
 }
--- HarmDemo/src/test/java/com/ConfHandler/HarmDemo/HarmDemoApplicationTests.java
+++ HarmDemo/src/test/java/com/ConfHandler/HarmDemo/HarmDemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.HarmBack;
+package com.ConfHandler.HarmDemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HarmBackApplicationTests {
+class HarmDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
