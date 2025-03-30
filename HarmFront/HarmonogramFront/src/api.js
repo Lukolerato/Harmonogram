771b8cad4ebd5a4762648ec4ecc421238f485a88
Dominika <dominika32110@gmial.com>
2025-03-30 17:43:37 +0200

    messagecomponent

---
+++ HarmFront/HarmonogramFront/src/api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:8080/api", // Adres backendu
+});
+
+export const getTasks = async () => {
+  const response = await api.get("/test");
+  return response.data;
+};
+
+export default api;
